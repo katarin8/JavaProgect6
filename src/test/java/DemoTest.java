7414d868c24abb19bd4ce117950abbf24783b409
katarin8 <katarin8@users.noreply.github.com>
2021-03-12 21:54:38 +0300

    Updated files

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -4,8 +4,8 @@ import org.junit.Test;
 public class DemoTest {
     @Test
     public void test1 () {
-        Road worker = new Road();
-        worker.setId(1);
-        System.out.println(worker.getId());
+        Road rd = new Road();
+        rd.setId(1);
+        System.out.println(rd.getId());
     }
 }
