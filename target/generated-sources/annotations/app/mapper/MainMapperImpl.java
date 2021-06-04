2397c0b07a6e3a577ba93416acf0916931fb82f5
katarin8 <katarin8@users.noreply.github.com>
2021-06-04 22:21:07 +0300

    Обновлена структура приложения, добавлены все необходимые зависимости

---
+++ target/generated-sources/annotations/app/mapper/MainMapperImpl.java
@@ -0,0 +1,13 @@
+package app.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-06-04T20:41:34+0300",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 15.0.2 (Oracle Corporation)"
+)
+@Component
+public class MainMapperImpl extends MainMapper {
+}
